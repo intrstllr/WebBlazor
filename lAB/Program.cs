6d99eeefdff16b97eb56768152644fd900964ba2
intrstllr <100162882+intrstllr@users.noreply.github.com>
2023-11-02 16:01:32 +0300

    laba 11

--- lAB/Program.cs
+++ lAB/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazorise.Icons.FontAwesome;
 using Blazorise.Bootstrap5;
+using Blazored.LocalStorage;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,7 +12,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddBlazoredLocalStorage();
 builder.Services
     .AddBlazorise(options =>
     {
